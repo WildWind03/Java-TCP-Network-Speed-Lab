ea0a7cb4de962232d26115db8820bf9bff8333c9
Alexander Chirikhin <usergreat03@gmail.com>
2016-10-08 20:06:02 +0700

    Add possibility to point IP in gradle build

--- src/main/java/ru/chirikhin/tcp2speed/client/Main.java
+++ src/main/java/ru/chirikhin/tcp2speed/client/Main.java
@@ -4,12 +4,15 @@ import org.apache.log4j.Logger;
 
 public class Main {
     private final static int SERVER_PORT = 1234;
-    private final static String DEFAULT_HOST = "127.0.0.1";
     private final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
         try {
-            Client client = new Client(DEFAULT_HOST, SERVER_PORT);
+            if (args.length > 1) {
+                throw new IllegalArgumentException("Too many args");
+            }
+
+            Client client = new Client(args[0], SERVER_PORT);
             Thread clientThread = new Thread(client);
             clientThread.start();
             clientThread.join();
